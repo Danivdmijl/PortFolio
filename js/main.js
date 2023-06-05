1c8495170a8f45801f24b347ea67de487fe96ff4
Dani Mijl van der <32570@ma-web.nl>
2023-06-05 15:36:33 +0200

    Finished HTML

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Test voor de JS");
+    // JavaScript function to create a scroll-to-top button
+    const scrollButton = document.getElementById('scrollButton');
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 200) {
+            scrollButton.style.display = 'block';
+        } else {
+            scrollButton.style.display = 'none';
+        }
+    });
+
+    scrollButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
