df168ea2b9a8d3d5974a0c3ee874de0e234b1505
Danivdmijl <32570@ma-web.nl>
2023-09-13 17:40:10 +0200

    changes modal

--- js/main.js
+++ js/main.js
@@ -18,27 +18,41 @@ scrollButton.addEventListener('click', function(e) {
     });
 });
 
-var modal = document.getElementById("myModal");
+// Get all the "View Project" buttons
+var openModalButtons = document.querySelectorAll('.open-modal-btn');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Add click event listeners to open the modals
+openModalButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        // Get the modal id from the data-modal-id attribute
+        var modalId = button.getAttribute('data-modal-id');
+        var modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "block";
+        }
+    });
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get all the close buttons within modals
+var closeButtons = document.querySelectorAll('.close');
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// Add click event listeners to close the modals
+closeButtons.forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+        var modal = closeButton.parentElement;
+        if (modal) {
+            modal.style.display = "none";
+        }
+    });
+});
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// When the user clicks anywhere outside of a modal, close it
+window.addEventListener('click', function(event) {
+    openModalButtons.forEach(function(button) {
+        var modalId = button.getAttribute('data-modal-id');
+        var modal = document.getElementById(modalId);
+        if (modal && event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
