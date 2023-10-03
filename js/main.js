92a1f934e73bd6eb86191a1236330f88fd89bce5
Danivdmijl <32570@ma-web.nl>
2023-10-03 12:13:52 +0200

    Added button modal + added animations

--- js/main.js
+++ js/main.js
@@ -1,56 +1,59 @@
 var openModalButtons = document.querySelectorAll('.open-modal-btn');
 
-        // Add click event listeners to open the modals
-        openModalButtons.forEach(function(button) {
-            button.addEventListener('click', function() {
-                var modalId = button.getAttribute('data-modal-id');
-                var modal = document.getElementById(modalId);
-                if (modal) {
-                    modal.style.display = "block";
-                } else {
-                    console.error("Modal not found for ID: " + modalId);
-                }
-            });
-        });
+// Add click event listeners to open the modals
+openModalButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        var modalId = button.getAttribute('data-modal-id');
+        var modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "block";
+        } else {
+            console.error("Modal not found for ID: " + modalId);
+        }
+    });
+});
 
-        // Get all the close buttons
-        var closeButtons = document.querySelectorAll('.close');
+// Get all the close buttons
+var closeButtons = document.querySelectorAll('.close');
 
-        // Add click event listeners to close the modals
-        closeButtons.forEach(function(closeButton) {
-            closeButton.addEventListener('click', function() {
-                var modalId = closeButton.getAttribute('data-modal-id');
-                var modal = document.getElementById(modalId);
-                if (modal) {
-                    modal.style.display = "none";
-                } else {
-                    console.error("Modal not found for ID: " + modalId);
-                }
-            });
-        });
+// Add click event listeners to close the modals
+closeButtons.forEach(function (closeButton) {
+    closeButton.addEventListener('click', function () {
+        var modalId = closeButton.getAttribute('data-modal-id');
+        var modal = document.getElementById(modalId);
+        if (modal) {
+            modal.style.display = "none";
+        } else {
+            console.error("Modal not found for ID: " + modalId);
+        }
+    });
+});
 
-        // When the user clicks anywhere outside of a modal, close it
-        window.addEventListener('click', function(event) {
-            openModalButtons.forEach(function(button) {
-                var modalId = button.getAttribute('data-modal-id');
-                var modal = document.getElementById(modalId);
-                if (modal && event.target === modal) {
-                    modal.style.display = "none";
-                }
-            });
-        });
-
+// When the user clicks anywhere outside of a modal, close it
+window.addEventListener('click', function (event) {
+    openModalButtons.forEach(function (button) {
+        var modalId = button.getAttribute('data-modal-id');
+        var modal = document.getElementById(modalId);
+        if (modal && event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+});
 
 const linkdinIMGs = document.querySelectorAll('.linkdin--button');
+const BigPic = document.getElementById("js--bigpicture");
 
 const linkdinURL = "https://www.linkedin.com/in/dani-van-der-mijl-163246232/";
 
-linkdinIMGs.forEach(function(linkdinIMG) {
-    linkdinIMG.addEventListener('click', function() {
+linkdinIMGs.forEach(function (linkdinIMG) {
+    linkdinIMG.addEventListener('click', function () {
         window.open(linkdinURL, '_blank');
     });
 });
 
+BigPic.addEventListener('click', function () {
+    window.open(linkdinURL, '_blank');
+});
 
 const githubIMGs = document.querySelectorAll('.github--button');
 
@@ -69,7 +72,7 @@ var githubURLs = [
     "https://github.com/Danivdmijl/CSS-Art-Flex/tree/main/Post%20its"
 ];
 
-githubIMGs.forEach(function(githubIMG, index) {
+githubIMGs.forEach(function (githubIMG, index) {
     githubIMG.addEventListener('click', function () {
         window.open(githubURLs[index], '_blank');
     });
@@ -80,8 +83,10 @@ githubIMGs.forEach(function(githubIMG, index) {
 
 
 // JavaScript for scroll-to-top button
-window.addEventListener('scroll', function() {
-    var scrollButton = document.querySelector('.scroll-button');
+var scrollButton = document.querySelector('.scroll-button');
+scrollButton.style.display = 'none'; // Start with the button hidden
+
+window.addEventListener('scroll', function () {
     if (window.scrollY > 200) {
         scrollButton.style.display = 'block';
     } else {
@@ -90,8 +95,7 @@ window.addEventListener('scroll', function() {
 });
 
 // Smooth scroll animation when button is clicked
-var scrollButton = document.querySelector('.scroll-button');
-scrollButton.addEventListener('click', function(e) {
+scrollButton.addEventListener('click', function (e) {
     e.preventDefault();
     window.scrollTo({
         top: 0,
